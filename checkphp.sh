ebdf2d508217f6a10bc739f9d7a9782caa46c82a
魏恒朝 <hengzhao.wei@qyer.com>
2019-03-18 16:18:47 +0800

    up

--- checkphp.sh
+++ checkphp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-git status|grep "modified" | awk '{cmd="php -l "$2};system(cmd)'
+#git提交前检测php语法，避免不小心引起线上问题
+
+git status|grep -E ".php$" | awk -F ":" '{cmd="php -l "$NF};system(cmd)'
