71b5c3dfe14033fd7fa3de855e85c4fda5e5f0d1
魏恒朝 <weihengzhao@shihuituan.com>
2020-06-22 15:40:00 +0800

    git push of http auto auth

--- gp.sh
+++ gp.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/expect -f
 set timeout 30
+#without prefix http://
 set host "your host"
 set username "your username"
 set password "your password"
 
 spawn git push
 expect {
-    "Username for '$host':" {
+    "Username for 'http://$host':" {
         send "$username\r"
         exp_continue;
     }
-    "Password for '$host':" {
+    "Password for 'http://$username@$host':" {
         send "$password\r"
     }
 }
